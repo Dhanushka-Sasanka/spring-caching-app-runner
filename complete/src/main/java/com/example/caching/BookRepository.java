cf4dbb290247d5cf0fc57c31dcbf534e1b539324
Dhanushka-Sasanka <dhanushkasasankapayza@gmail.com>
2022-09-09 03:23:44 +0530

    initial commit

--- complete/src/main/java/com/example/caching/BookRepository.java
+++ complete/src/main/java/com/example/caching/BookRepository.java
@@ -1,5 +1,8 @@
 package com.example.caching;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface BookRepository {
 
 	Book getByIsbn(String isbn);
--- complete/src/main/java/com/example/caching/SimpleBookRepository.java
+++ complete/src/main/java/com/example/caching/SimpleBookRepository.java
@@ -1,13 +1,17 @@
 package com.example.caching;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleBookRepository implements BookRepository {
 
+	private static final Logger logger = LoggerFactory.getLogger(SimpleBookRepository.class);
+
 	@Override
-	@Cacheable("books")
+	@Cacheable(value = "books" , unless = "#isbn == 'isbn-4567'")
 	public Book getByIsbn(String isbn) {
 		simulateSlowService();
 		return new Book(isbn, "Some book");
@@ -16,7 +20,8 @@ public class SimpleBookRepository implements BookRepository {
 	// Don't do this at home
 	private void simulateSlowService() {
 		try {
-			long time = 3000L;
+			long time = 5000L;
+			logger.info("ERROR");
 			Thread.sleep(time);
 		} catch (InterruptedException e) {
 			throw new IllegalStateException(e);
